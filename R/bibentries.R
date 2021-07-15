fab1fda904b41663f81288683fb708b61be7b311
Marc Becker <marcbecker@posteo.de>
2021-07-15 19:10:57 +0200

    update references

--- R/bibentries.R
+++ R/bibentries.R
@@ -1,16 +1,20 @@
 #' @importFrom utils bibentry
 bibentries = c(
-  bischl_2021 = bibentry("article",
-    title        = "A Practical Introduction into Hyperparameter Optimization: Algorithms, Software, Best Practices and Open Challenges",
-    author       = "Bernd Bischl and Martin Binder and Michel Lang and Tobias Pielok and Jakob Richter and Stefan Coors and Janek Thomas and Theresa Ullmann and Marc Becker and Anne-Laure Boulesteix",
-    year         = "2021",
-    journal      = "arvixs"
+  bischl_2021 = bibentry("misc",
+    title         = "A Practical Introduction into Hyperparameter Optimization: Algorithms, Software, Best Practices and Open Challenges",
+    author        = "Bernd Bischl and Martin Binder and Michel Lang and Tobias Pielok and Jakob Richter and Stefan Coors and Janek Thomas and Theresa Ullmann and Marc Becker and Anne-Laure Boulesteix and Difan Deng and Marius Lindauer",
+    year          = "2021",
+    eprint        = "2107.05847",
+    archivePrefix = "arXiv",
+    primaryClass  = "stat.ML"
   ),
 
-  kuehn_2018 = bibentry("article",
-    title        = "Automatic Exploration of Machine Learning Experiments on OpenML",
-    author       = "Daniel Kuehn and Philipp Probst and Janek Thomas and Bernd Bischl",
-    year         = "2018",
-    journal      = "arvixs"
+  kuehn_2018 = bibentry("misc",
+    title         = "Automatic Exploration of Machine Learning Experiments on OpenML",
+    author        = "Daniel Kuehn and Philipp Probst and Janek Thomas and Bernd Bischl",
+    year          = "2018",
+    eprint        = "1806.10961",
+    archivePrefix = "arXiv",
+    primaryClass  = "stat.ML"
   )
 )
