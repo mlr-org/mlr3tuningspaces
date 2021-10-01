c96f2e25457bbefd344f1916590443ea441f0958
Michel Lang <michellang@gmail.com>
2021-10-01 16:14:49 +0200

    fix citation

--- R/bibentries.R
+++ R/bibentries.R
@@ -1,12 +1,13 @@
 #' @importFrom utils bibentry
 bibentries = c(
   bischl_2021 = bibentry("misc",
-    title         = "A Practical Introduction into Hyperparameter Optimization: Algorithms, Software, Best Practices and Open Challenges",
+    title         = "Hyperparameter Optimization: Foundations, Algorithms, Best Practices and Open Challenges",
     author        = "Bernd Bischl and Martin Binder and Michel Lang and Tobias Pielok and Jakob Richter and Stefan Coors and Janek Thomas and Theresa Ullmann and Marc Becker and Anne-Laure Boulesteix and Difan Deng and Marius Lindauer",
     year          = "2021",
     eprint        = "2107.05847",
     archivePrefix = "arXiv",
-    primaryClass  = "stat.ML"
+    primaryClass  = "stat.ML",
+    url = "https://arxiv.org/abs/2107.05847"
   ),
 
   kuehn_2018 = bibentry("misc",
@@ -15,6 +16,7 @@ bibentries = c(
     year          = "2018",
     eprint        = "1806.10961",
     archivePrefix = "arXiv",
-    primaryClass  = "stat.ML"
+    primaryClass  = "stat.ML",
+    url = "https://arxiv.org/abs/1806.10961"
   )
 )
