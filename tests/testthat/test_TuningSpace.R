0392de23249a7a1484cf1bd8ac89248040d4e97b
be-marc <marcbecker@posteo.de>
2022-04-12 08:40:44 +0200

    test: as.data.table.TuningSpace

--- tests/testthat/test_TuningSpace.R
+++ tests/testthat/test_TuningSpace.R
@@ -3,3 +3,12 @@ test_that("get_learner function works", {
   expect_learner(learner)
   expect_equal(learner$predict_type, "prob")
 })
+
+test_that("as.dat.table.TuningSpace works", {
+  keys =  mlr_tuning_spaces$keys()
+  walk(keys, function(key) {
+    tab = as.data.table(lts(key))
+    expect_data_table(tab)
+    expect_names(names(tab), must.include = c("id"))
+  })
+})
