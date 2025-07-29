c9c09fea42e81768e60cfe816875012ff81ad0e6
Carson Zhang <carsonzhang4@gmail.com>
2025-07-29 13:26:15 +0200

    uncomment helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -6,7 +6,7 @@ library(mlr3torch)
 library(paradox)
 library(R6)
 
-# lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
 # nolint end
 
 test_tuning_space = function(key, learner) {
